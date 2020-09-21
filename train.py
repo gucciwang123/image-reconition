3e55b765695e10accd00df21b03ae7907d3715ee
Tyler Wang <wangtyler123@gmail.com>
2020-09-21 11:18:39 -0500

    Finished; need work on pausing graphing and resuming runs

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
+import time
 
 #information about network to be trained
-numOfPixles = 100 #change this value to for different number of pixles
-numOfNodes = [numOfPixles, 20, 20, 10] #length must match number of layers
+numOfPixles = 5 #change this value to for different number of pixles
+numOfNodes = [numOfPixles, 10, 20, 1] #length must match number of layers
 numOfLayers = 4 #change this for different number of layers in neural network
-learningrate = 0.1 #change this value to receive different leaning rates
+learningrate = 0.5 #change this value to receive different leaning rates
 
 #loging
 logging = False;
@@ -23,13 +24,13 @@ def stigmoid(x):
 vector_stigmoid = np.vectorize(stigmoid)
 
 #init weights and biases
-weights = [(np.random.rand(numOfNodes[x + 1], numOfNodes[x]) - 0.5) * 12 for x in range(0, numOfLayers - 1)]
+weights = [(np.random.rand(numOfNodes[x], numOfNodes[x - 1]) - 0.5) * 12 for x in range(1, numOfLayers)]
 biases = [(np.random.rand(numOfNodes[x]) - 0.5) * 12 for x in range(1, numOfLayers)]
 
 def runNeuralNetwork(input): #give pixle values in 32-bit floats. Length must mach numOfPixles
     nodeValues = [np.array(input)]
     for x in range(0, numOfLayers - 1):
-        nodeValues.append(vector_stigmoid(np.add(np.matmul(weights[x], nodeValues) + biases[x])))
+        nodeValues.append(vector_stigmoid(np.matmul(weights[x], nodeValues[x]) + biases[x]))
     return nodeValues
 
 #backpropagation
@@ -56,14 +57,30 @@ def backpropagation(nodeValues, expectedValues):
 
     for x in range(0, numOfLayers - 1):
         g_node *= vector_d_stigmoid(vector_r_stigmoid(nodeValues[-1 * (x + 1)]))
-        g_biases.append(np.array(g_node))
-        g_weights.append(np.resize(nodeValues[-1 * (x + 1)], (numOfNodes[-1 * (x + 1)], numOfNodes[-1 * (x + 2)])) *
-                         np.transpose(np.resize(nodeValues[-1 * (x + 1)], (numOfNodes[-1 * (x + 2)], numOfNodes[-1 * (x + 1)]))))
+        g_biases.insert(0, np.array(g_node))
+        g_weights.insert(0,
+                         np.transpose(np.resize(g_node, (numOfNodes[-1 * (x + 2)], numOfNodes[-1 * (x + 1)]))) *
+                         np.resize(nodeValues[-1 * (x + 1)], (numOfNodes[-1 * (x + 1)], numOfNodes[-1 * (x + 2)]))
+                         )
         g_node = np.matmul(np.transpose(weights[-1 * (x + 1)]), g_node)
+
     return gradient
+
+def applyGradient(weights_gradient, bias_gradient):
+    for x in range(0, numOfLayers - 1):
+        weights[x] +=  weights_gradient[x] * learningrate
+        biases[x] += bias_gradient[x] * learningrate
 
 #script
 logging = True
-for x in range(-6, 6):
-    log(stigmoid(x))
+
+for x in range(0, 10000):
+    output = runNeuralNetwork([0.3, 0.2, 0.5, 0.2, 0.8])
+    gradientVector = backpropagation(output, np.array([.5]))
+    for y in output[3].tolist():
+        print(str(x) + ": Value of output = "+ str(y))
+    log(str(x) + ": Cost of run: " + str(cost(output[3].tolist()[0], 0.5)))
+    log("_____________________________________")
+    applyGradient(gradientVector[0], gradientVector[1])
+
 
