f1b4870f8f873f6d9807eace22597196c51ff146
Tyler Wang <wangtyler123@gmail.com>
2020-09-22 12:22:07 -0500

    Added new stuff to Evaluate

--- Evaluate.py
+++ Evaluate.py
@@ -23,8 +23,42 @@ def runNeuralNetwork(input): #give pixle values in 32-bit floats. Length must ma
 
 #data parsing code
 def data():
-    #function logic goes here
+    f_images = open("Images/images", "rb")
+    f_lables = open("Images/labels", "rb")
+
+    f_images.seek(16, 0)
+    f_lables.seek(8, 0)
+
+    images = []
+    lables = []
+
+    # parsing images
+    buffer = struct.unpack("B" * (28 * 28 * 60000), f_images.read())
+    for x in range(0, 60000):
+        images.append(buffer[x * 28 * 28: x * 28 * 28 + 28 * 28])
+    f_images.close()
+
+    # parsing lables
+    buffer = struct.unpack("b" * (60000), f_lables.read())
+    for x in range(0, 60000):
+        lables.append(buffer[x])
+    f_lables.close()
+
+    return (images, lables)
 
 #AI scrip goes here
 def run():
     #function logic goes here
+    images, lables = data()
+    buffer = np.array(images[x * runsPerEvolution + y], np.float) * 1.0 / 256
+    nodeActivation = runNeuralNetwork(buffer)
+    expected[lables[x * runsPerEvolution + y]] = 1
+
+    print("Cost for run: " + str(
+        np.dot(np.full((numOfNodes[-1]), 1.0 / numOfNodes[-1]), vector_cost(nodeActivation[-1], np.array(expected)))))
+
+    print("\t\tAnswer: " + str((nodeActivation[-1].tolist()).index(max(nodeActivation[-1].tolist()))))
+    if (nodeActivation[-1].tolist()).index(max(nodeActivation[-1].tolist())) == lables[x * runsPerEvolution + y]:
+        print("Correct")
+    else:
+        print("No")
